a49662c4221273507a93baea98ba37d3bfa2ca6a
Gwyn <gwyn@playtechnique.io>
2022-03-29 10:05:44 -0600

    Disables repo check for centos7 per upstream docs
    https://cloud.google.com/compute/docs/troubleshooting/known-issues#keyexpired

--- setup-kubetools.sh
+++ setup-kubetools.sh
@@ -19,13 +19,14 @@ OSVERSION=$(hostnamectl | awk '/Operating/ { print $4 }')
 if [ $MYOS = "CentOS" ]
 then
 	echo RUNNING CENTOS CONFIG
+	# Repo check turned off per https://cloud.google.com/compute/docs/troubleshooting/known-issues#keyexpired
 	cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
 enabled=1
 gpgcheck=1
-repo_gpgcheck=1
+repo_gpgcheck=0
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 exclude=kubelet kubeadm kubectl
 EOF
