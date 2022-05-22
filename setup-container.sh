562e9d757d75141a0d6dd512e9decaf44ee38aba
Gwyn <gwyn@playtechnique.io>
2022-05-21 21:39:37 -0600

    Supports kubernetes 1.24
    Also sort of dope is making the script simpler by removing ubuntu
    support and adding specific links to references for how-to instructions.

--- setup-container.sh
+++ setup-container.sh
@@ -1,90 +1,104 @@
-#!/bin/bash
+#!/bin/bash -el
 # script that runs 
-# https://kubernetes.io/docs/setup/production-environment/container-runtime
+# https://kubernetes.io/docs/setup/production-environment/container-runtimes
 
-# setting MYOS variable
-MYOS=$(hostnamectl | awk '/Operating/ { print $3 }')
-OSVERSION=$(hostnamectl | awk '/Operating/ { print $4 }')
-
-##### CentOS 7 config
-if [ $MYOS = "CentOS" ]
-then
-	echo setting up CentOS 7 with Docker 
-	yum install -y vim yum-utils device-mapper-persistent-data lvm2
-	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
-	# notice that only verified versions of Docker may be installed
-	# verify the documentation to check if a more recent version is available
-
-	yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-	[ ! -d /etc/docker ] && mkdir /etc/docker
-
-	mkdir -p /etc/systemd/system/docker.service.d
-
-
-	cat > /etc/docker/daemon.json <<- EOF
-	{
-	  "exec-opts": ["native.cgroupdriver=systemd"],
-	  "log-driver": "json-file",
-	  "log-opts": {
-	    "max-size": "100m"
-	  },
-	  "storage-driver": "overlay2",
-	  "storage-opts": [
-	    "overlay2.override_kernel_check=true"
-	  ]
-	}
-
-	EOF
-
-# Starting in version 1.24, I have been getting an error from kubeadm. The error is:
-# # kubeadm init --apiserver-advertise-address 192.168.56.110
-# [init] Using Kubernetes version: v1.24.0
-# [preflight] Running pre-flight checks
-# error execution phase preflight: [preflight] Some fatal errors occurred:
-#	[ERROR CRI]: container runtime is not running: output: time="2022-05-18T03:00:30Z" level=fatal msg="getting status of runtime: rpc error: code = Unimplemented desc = unknown service runtime.v1alpha2.RuntimeService"
-#, error: exit status 1
-# I looked at installing the docker Mirantis tooling and it seemed like a pain, so let's try containerd instead.
-# To use containerd, we need to install the yum packets above, and then make sure that 'cri' is not a disabled container runtime.
-	sed -i 's/disabled_plugins = \["cri"]/disabled_plugins = [""]/' /etc/containerd/config.toml
-
-	systemctl daemon-reload
-	systemctl restart docker
-	systemctl enable docker
-
-	systemctl disable --now firewalld
-fi
-
-echo printing MYOS $MYOS
-
-if [ $MYOS = "Ubuntu" ]
-then
-	### setting up container runtime prereq
-	cat <<- EOF | sudo tee /etc/modules-load.d/containerd.conf
+# https://kubernetes.io/docs/setup/production-environment/container-runtimes/#forwarding-ipv4-and-letting-iptables-see-bridged-traffic
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 	overlay
 	br_netfilter
-	EOF
+EOF
 
-	sudo modprobe overlay
-	sudo modprobe br_netfilter
+modprobe overlay
+modprobe br_netfilter
 
-	# Setup required sysctl params, these persist across reboots.
-	cat <<- EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
-	net.bridge.bridge-nf-call-iptables  = 1
-	net.ipv4.ip_forward                 = 1
-	net.bridge.bridge-nf-call-ip6tables = 1
-	EOF
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
 
-	# Apply sysctl params without reboot
-	sudo sysctl --system
+# Apply sysctl params without reboot
+sysctl --system
+# /https://kubernetes.io/docs/setup/production-environment/container-runtimes/#forwarding-ipv4-and-letting-iptables-see-bridged-traffic
 
-	# (Install containerd)
-	sudo apt-get update && sudo apt-get install -y containerd
-	# Configure containerd
-	sudo mkdir -p /etc/containerd
-	containerd config default | sudo tee /etc/containerd/config.toml
-	# Restart containerd
-	sudo systemctl restart containerd	
-fi
+# The next section is https://kubernetes.io/docs/setup/production-environment/container-runtimes/#cgroup-drivers
+# This is configuring the cgroup driver. This is accomplished with the setting "native.cgroupdriver=systemd"
+# when it is handed into docker via daemon.json below.
 
+
+# The next section is CRI Runtimes.
+# We are going to use 
+# The container-runtimes article points to https://github.com/containerd/containerd/blob/main/docs/getting-started.md
+# to set up docker-engine, so we can use docker images.
+
+# First install docker-engine.
+# Then install cri-dockerd.
+echo setting up CentOS 7 with Docker\-Engine 
+echo "Instructions at https://docs.docker.com/engine/install/#server"
+yum install -y vim yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# notice that only verified versions of Docker may be installed
+# verify the documentation to check if a more recent version is available
+yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+systemctl daemon-reload
+systemctl enable --now containerd
+
+
+[ ! -d /etc/docker ] && mkdir /etc/docker
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+
+cat > /etc/docker/daemon.json <<- EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+
+EOF
+
+systemctl enable docker
+systemctl start docker
+
+# We need to manually install dockershim, which is now called cri-dockerd.
+# https://github.com/Mirantis/cri-dockerd
+
+# To do that, we need wget to install golang, git to get the go code for cri-dockerd and golang.
+# Let's fetch those up here
+yum install -y wget git 
+echo 'export GOPATH=/root/golang/' >> /root/.bash_profile
+echo 'export GOPATH=/home/vagrant/golang/' >> /vagrant/.bash_profile
+
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /root/.bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /vagrant/.bash_profile
+
+source /root/.bash_profile
+
+wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
+
+git clone https://github.com/Mirantis/cri-dockerd.git
+cd cri-dockerd/src && go get && go build -o ../bin/cri-dockerd
+
+cd ..
+
+install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
+cp -a packaging/systemd/* /etc/systemd/system
+sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+systemctl daemon-reload
+systemctl enable cri-docker.service
+systemctl enable --now cri-docker.socket
+
+systemctl restart containerd
+systemctl restart docker
+
+systemctl disable --now firewalld
